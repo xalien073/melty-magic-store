6fb1ce850736b19071cec6f5dfc5a50c41e8931b
xalien073 <95920108+xalien073@users.noreply.github.com>
2025-04-06 12:06:47 +0530

    Fixed typo in layout.js!

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "./providers"; // Import the client component
+import Providers from "./providers"; // Import the client component, fixed the typo!
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
